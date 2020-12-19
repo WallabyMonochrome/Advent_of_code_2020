eeba9bd7c7eaa73973d1185bb335a9d3f9070e5e
Thib <thibault.introvigne@scorechain.com>
2020-12-19 20:38:32 +0100

    day 19 part 2 ez

--- day19/solve.js
+++ day19/solve.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 
 const [rules, data] = fs.readFileSync('input.txt', 'utf8').split(/\n\n/g);
-// console.log(rules);
 const dictRules = {};
 
 function registerAllRules() {
@@ -9,63 +8,62 @@ function registerAllRules() {
        let [ruleNumber, ruleDesc] = r.split(': ');
         let ruleObject = {
             ruleDesc,
-            matchRules: (data) => {
-                // if(!data.length) return {sucess: true, data: []}
-                // console.log('LEs data', data, ruleNumber);
-
-                if(ruleDesc === "\"a\"" || ruleDesc === "\"b\"") {
-                    if(data[0] === ruleDesc.replace(/\"/g,'')) {
-                        return {sucess: true, data: data.slice(1)}
-                    }
-                        return {sucess: false};
-                    }
-                    if(ruleDesc.split('|').length > 1) {
-                        let [first, second] = ruleDesc.split('|');
-                        let dataBisFirst = data;
-                        let isValidFirst = true;
-                        first = first.match(/\d+/g)
-                        first.forEach((r) => {
-                            if(isValidFirst){
-                                let result = dictRules[r].matchRules(dataBisFirst);
-                                if(!result.sucess) isValidFirst = false;
-                                dataBisFirst = result.data;
-                            }
-                        });
-
-                // if(ruleNumber === '8' || ruleNumber === '41') {
-                //     console.log('LEs data',isValidFirst,  data, ruleNumber);
-                // }
-                        // console.log('Loo',isValidFirst,  data, ruleNumber);
-                        if(isValidFirst) return {sucess: true, data: dataBisFirst};
-                        let isValidSecond = true;
-                        let dataBisSecond = data;
-                        second = second.match(/\d+/g)
-                        second.forEach((r) => {
-                            if(isValidSecond){
-                                let result = dictRules[r].matchRules(dataBisSecond);
-                                if(!result.sucess) isValidSecond = false;
-                                dataBisSecond = result.data;
-                            }
-                        });
-                        if(isValidSecond) return {sucess: true, data: dataBisSecond};
-                        return {sucess: false};
-                    }
-                    let rules = ruleDesc.match(/\d+/g)
-                    let dataBis = data;
-                    let isValid = true;
-                    rules.forEach((r) => {
-                        if(isValid){
-                            let result = dictRules[r].matchRules(dataBis);
-                            if(!result.sucess) isValid = false;
-                            dataBis = result.data;
-                        }
-                    });
-                    if(isValid) return { sucess: true, data: dataBis};
-                    return {sucess: false};
-            }
         }
-        dictRules[ruleNumber] = ruleObject;
+        dictRules[ruleNumber] = ruleDesc;
     });
+}
+
+function computeRule(data){
+    let ruleIndex = 0;
+    let currentData = data;
+    let checkpoint = [];
+    let evaluating = [];
+    // Init
+    if(dictRules[ruleIndex].split('|').length > 1) {
+        let [first, second] = ruleDesc.split('|');
+        first = first.match(/\d+/g);
+        second = second.match(/\d+/g);
+        evaluating.unshift(...first);
+        checkpoint.push({data: [...currentData], rule:[...second]});
+    } else {
+        let rules = dictRules[ruleIndex].match(/\d+/g);
+        evaluating.unshift(...rules);
+    }
+    // End init
+    while(true) {
+        if(!evaluating.length && !currentData.length) {
+            return true;
+        }
+        if(!evaluating.length) {
+            if(!checkpoint.length) {return false}
+                currentData = checkpoint[0].data;
+                evaluating = checkpoint[0].rule;
+                checkpoint.shift();
+        }
+        let currentRule = dictRules[evaluating[0]];
+        evaluating.shift();
+        if(currentRule === "\"a\"" || currentRule === "\"b\"") {
+            if(currentData[0] === currentRule.replace(/\"/g,'')) {
+                currentData = currentData.slice(1);
+            } else {
+                if(!checkpoint.length) {return false}
+                currentData = checkpoint[0].data;
+                evaluating = checkpoint[0].rule;
+                checkpoint.shift();
+            }
+    } else {
+        if(currentRule.split('|').length > 1) {
+            let [first, second] = currentRule.split('|');
+            first = first.match(/\d+/g);
+            second = second.match(/\d+/g);
+            checkpoint.push({data: [...currentData], rule: [...second, ... evaluating]});
+            evaluating.unshift(...first);
+        } else {
+            let rules = currentRule.match(/\d+/g);
+            evaluating.unshift(...rules);
+        }
+    }
+}
 }
 
 function checkData() {
@@ -73,11 +71,8 @@ function checkData() {
     let input = data.split(/\n/g);
     input.forEach((d) => {
         let dataS = d.split('');
-        let result = dictRules[0].matchRules(dataS);
-        console.log('Le result' , result);
-        if(result.sucess && result.data.length > 0) result.sucess = false;
-        if(result.sucess) correctRule++;
-//        if(result.sucess) console.log('Data is correct: ', d);
+        let result = computeRule(dataS);
+        if(result) correctRule++;
     });
     console.log('Correct rule: ', correctRule);
 }
